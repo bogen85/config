4ec6abd2b7ef35b2d90e54c3d010bdf7acc12f31
bogen85 <bogen85@teegra.net>
2025-09-01 17:52:59 -0500

    Renamed goscripter.go to main.go in lieu of factoring out a bulk of the code

--- dot.go/goscripter/build.sh
+++ dot.go/goscripter/build.sh
@@ -1,1 +1,1 @@
-(set -x; GO111MODULE=auto GOPATH=/usr/share/gocode go build -trimpath -ldflags="-s -w"   goscripter.go)
+(set -x; GO111MODULE=auto GOPATH=/usr/share/gocode go build -trimpath -ldflags="-s -w"  -o goscripter main.go)
