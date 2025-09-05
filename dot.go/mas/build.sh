3e7e703e1b8dec441aad76adb1ef338206835696
bogen85 <bogen85@teegra.net>
2025-09-05 02:21:47 -0500

    More build commands

--- dot.go/mas/build.sh
+++ dot.go/mas/build.sh
@@ -3,8 +3,10 @@ set -xeuo pipefail &&
 rm -vf masuds &&
 export GO111MODULE=auto &&
 export GOPATH=/usr/share/gocode &&
+export CGO_ENABLED=0
 export SECRET=$(openssl rand -hex 16) &&
 echo $SECRET &&
-go build -ldflags "-s -w -X 'main.buildSecretHex=$SECRET' -X 'main.buildID=dev-uds'" -o masuds masuds.go &&
+go build -tags 'osusergo,netgo' -ldflags "-s -w -X 'main.buildSecretHex=$SECRET' -X 'main.buildID=dev-uds'" -o masuds masuds.go &&
 cp masuds /home/dev/test/
+strip -v /home/dev/test/masuds
 )
